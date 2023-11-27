19d8d876e09a6dc77c1cebd3a7d067b217bbdd22
rajkumarrraja <110884663+rajkumarrraja@users.noreply.github.com>
2023-11-27 16:52:18 +0530

    add template

---
+++ src/component/Cart.jsx
@@ -0,0 +1,83 @@
+import '../index.css';
+import { useState, useEffect } from 'react';
+
+const Cart = () => {
+  const [cartData, setCartData] = useState('');
+
+  useEffect(() => {
+    fetch('https://reqres.in/api/users/1')
+      .then((data) => data.json())
+      .then((data) => dataFetch(data.data));
+  }, []);
+
+  function dataFetch(params) {
+    setCartData(params);
+    console.log(params);
+  }
+
+  return (
+    <div className='cart'>
+      <div className='cartInside'>
+        <div className='cartImg'>
+          <img src={cartData.avatar} alt={cartData.first_name} />
+        </div>
+        <div className='tab'>
+          {/* <h3 className='cartName'>{cartData.first_name}</h3> */}
+          <div className='cartText'>
+            <h3 className='cartName'>{cartData.first_name}</h3>
+            <div className='cartContent'>
+              <h4>Email</h4>
+              <p>{cartData.email}</p>
+            </div>
+            <div className='cartContent'>
+              <h4>Occupation</h4>
+              <p>Web Developer</p>
+            </div>
+            <div className='cartContent'>
+              <h4>Location</h4>
+              <p>Coimbatore</p>
+            </div>
+            <div className='cartContent4'>
+              <h4>Address</h4>
+              <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Est,
+                perspiciatis minus. Rerum iure rem nemo hic dolorem repellendus
+                earum iste id eos repudiandae assumenda
+              </p>
+            </div>
+          </div>
+        </div>
+        {/* <div className='lap'>
+          <div className='cartTextTab'>
+            <div>
+              <div className='cartContentTab'>
+                <h4>Email</h4>
+                <p>{cartData.email}</p>
+              </div>
+              <div className='cartContent'>
+                <h4>Occupation</h4>
+                <p>Web Developer</p>
+              </div>
+            </div>
+            <div>
+              <div className='cartContent'>
+                <h4>Location</h4>
+                <p>Coimbatore</p>
+              </div>
+              <div className='cartContent4'>
+                <h4>Address</h4>
+                <p>
+                  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Est,
+                  perspiciatis minus. Rerum iure rem nemo hic dolorem
+                  repellendus earum iste id eos repudiandae assumenda
+                </p>
+              </div>
+            </div>
+          </div>
+        </div> */}
+      </div>
+    </div>
+  );
+};
+
+export default Cart;
